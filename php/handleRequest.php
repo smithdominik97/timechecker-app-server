ef2f82c0bc20bf3c9487b10b848d288bdbf3986b
Dominik Smith <smithdominik97@gmail.com>
2024-04-14 23:06:34 +0100

    changed files to debug

--- php/handleRequest.php
+++ php/handleRequest.php
@@ -6,10 +6,16 @@ include 'connection.php';
 //new change
 // $departure = $_POST['departure'];
 // $destination = $_POST['destination'];
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $departure = "DEE";
 $destination = "EGY";
 $conn = new OpenLDBWSConnection();
+var_dump($conn);
 $OpenLDBWS = $conn->connect();
+var_dump($OpenLDBWS);
 $response = $OpenLDBWS->GetArrivalDepartureBoard(10, $departure, array($destination));
 header("Content-Type: text/plain");
 print_r($response);
