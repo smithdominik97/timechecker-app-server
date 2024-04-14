7c4aa385f450aa74c94152206c02a7ee9afaf8ed
Dominik Smith <smithdominik97@gmail.com>
2024-04-14 23:03:46 +0100

    added missing file

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,23 @@
 <?php
+
+include 'OpenLDBWS.php';
 //new change
+class OpenLDBWSConnection {
+
+  private $accessToken;
+
+  function __construct() {
+    $this->accessToken = "c69fed7e-e5f5-4ad0-9517-0c73ad909809";
+  }
+
+ public function connect() {
+      $OpenLDBWS = new OpenLDBWS($this->accessToken);
+      return $OpenLDBWS;
+  }
+}
 
 
-$OpenLDBWS = new OpenLDBWS("c69fed7e-e5f5-4ad0-9517-0c73ad909809");
-return $OpenLDBWS;
+
+
+
 ?>
