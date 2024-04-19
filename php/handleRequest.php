2b6ac5c6e8ddf2261b0d6a5dc8532459ca9d18a6
Dominik Smith <smithdominik97@gmail.com>
2024-04-19 14:32:31 +0100

    adjusted code to display departure time and arrival time

--- php/handleRequest.php
+++ php/handleRequest.php
@@ -6,19 +6,28 @@ include 'connection.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$departure = "DEE";
-$destination = "EGY";
+//$departure = "DEE";
+//$destination = "EGY";
 $conn = new OpenLDBWSConnection();
 $OpenLDBWS = $conn->connect();
 // $response = $OpenLDBWS->GetArrivalDepartureBoard(10, "EGY", array("EGY"));
-// $response = $OpenLDBWS->GetDepartureBoard(10,"DEE");
-$response = $OpenLDBWS->GetNextDepartures("DEE", "EGY", 120, 120);
+$response = $OpenLDBWS->GetDepartureBoard(10,"DEE");
+$departures = $OpenLDBWS->GetDepBoardWithDetails(1,"DEE", "EGY");
+$arrivals = $OpenLDBWS->GetArrBoardWithDetails(1, "EGY");
+
+$serviceId = null;
+
+//$response = $OpenLDBWS->GetNextDepartures("DEE", "EGY", 120, 120);
 header("Content-Type: text/plain");
 
 if($response == null){
     echo "No data found";
     return;
 } else {
-    print_r($response);
-}
+    print_r($departures);
+    echo "Departure: \n";
+    echo "  Departure from: " . $departures->GetStationBoardResult->locationName . " leaves at: " . $departures->GetStationBoardResult->trainServices->service[0]->std;
+    echo "\nArrival: \n"; 
+    echo "  Arrival from: " . $departures->GetStationBoardResult->locationName . " arrives at: " . $departures->GetStationBoardResult->trainServices->service[0]->subsequentCallingPoints->callingPointList[0]->callingPoint[10]->st; 
+  }
 
