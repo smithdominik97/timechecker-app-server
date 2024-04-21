1c99db9a0d0e744cce251ddf004aabf76a7cf2fb
Dominik Smith <smithdominik97@gmail.com>
2024-04-21 16:56:48 +0100

    removed error reporting

--- php/handleRequest.php
+++ php/handleRequest.php
@@ -2,9 +2,7 @@
 include 'connection.php';
 
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 $departure = $_POST['departure'];
 $destination = $_POST['destination'];
